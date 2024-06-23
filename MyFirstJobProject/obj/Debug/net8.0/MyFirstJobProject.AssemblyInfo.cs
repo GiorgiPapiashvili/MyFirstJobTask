3e3cddf1484ebabcce7cdf6b34997baf476f1092
Giorgi Papiashvili <Giushki77@gmail.com>
2024-06-23 16:12:03 +0400

    some minor fixses

--- MyFirstJobProject/obj/Debug/net8.0/MyFirstJobProject.AssemblyInfo.cs
+++ MyFirstJobProject/obj/Debug/net8.0/MyFirstJobProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e08bbece8ef38ab0a4a4a66e98688c9c5e9c1f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184a263122944ce02b8c41efada96038e9cfbd03")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
