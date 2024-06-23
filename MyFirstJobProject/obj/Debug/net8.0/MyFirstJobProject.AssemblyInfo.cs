ed872dd21eb8dd9acd35b69c94eba47d72c90582
Giorgi Papiashvili <Giushki77@gmail.com>
2024-06-23 16:13:56 +0400

    marge main to workbranch

--- MyFirstJobProject/obj/Debug/net8.0/MyFirstJobProject.AssemblyInfo.cs
+++ MyFirstJobProject/obj/Debug/net8.0/MyFirstJobProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e08bbece8ef38ab0a4a4a66e98688c9c5e9c1f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5adf0d585338e5089482284f50f19b8af31bdca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstJobProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
